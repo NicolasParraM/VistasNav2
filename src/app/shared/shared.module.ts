7e45b5f8ec4525dcfeffca719e46e8a8a24ad68f
NicolasP <Nicolasandresparra123@gmail.com>
2022-09-02 05:34:15 -0500

    Navbar y Sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-
-
 
 @NgModule({
   declarations: [
-    NavbarComponent,
-    SidebarComponent
+    NavbarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent,
-    SidebarComponent
+    NavbarComponent
   ]
 })
 export class SharedModule { }
